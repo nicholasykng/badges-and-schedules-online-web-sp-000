4e81fd93fd50d4230d57b9806b65d0d4c658b317
Nicholas Ng <nicholasykng@gmail.com>
2019-10-19 02:05:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  presentation = []
+  array.each do |name|
+    presentation << badge_maker(name)
+  end
+  return presentation
+end
